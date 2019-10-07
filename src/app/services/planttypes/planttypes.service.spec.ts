3ffbb929ffb937b29d93846d8c00542a42fb4a7b
fredvdvd <fredvdvd@gmail.com>
2019-10-07 10:19:45 +0200

    add planttype service

---
+++ src/app/services/planttypes/planttypes.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { PlanttypesService } from './planttypes.service';
+
+describe('PlanttypesService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: PlanttypesService = TestBed.get(PlanttypesService);
+    expect(service).toBeTruthy();
+  });
+});
