96b1d9fd2322b97486c70e0fe9bf9a93d5b39fe7
fredvdvd <fredvdvd@gmail.com>
2019-10-09 18:58:29 +0200

    responsiveness

--- src/app/plants/plant-types-new/plant-types-new.component.ts
+++ src/app/plants/plant-types-new/plant-types-new.component.ts
@@ -1,44 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { PlanttypesService } from "../../services/planttypes/planttypes.service";
-import {Router} from '@angular/router';
-import { PlantType } from 'src/app/shared/models/plant-type';
-import { faTree} from '@fortawesome/free-solid-svg-icons';
+import { Router } from "@angular/router";
+import { PlantType } from "src/app/shared/models/plant-type";
+import { faTree } from "@fortawesome/free-solid-svg-icons";
 @Component({
-  selector: 'app-plant-types-new',
-  templateUrl: './plant-types-new.component.html',
-  styleUrls: ['./plant-types-new.component.scss']
+  selector: "app-plant-types-new",
+  templateUrl: "./plant-types-new.component.html",
+  styleUrls: ["./plant-types-new.component.scss"]
 })
 export class PlantTypesNewComponent implements OnInit {
-  moistValue:number=0;
-  sunValue:number=0;
-  frostValue:number = 0;
-  fireValue:number = 0;
+  moistValue: number = 0;
+  sunValue: number = 0;
+  frostValue: number = 0;
+  fireValue: number = 0;
   faTree = faTree;
   plantType: PlantType = new PlantType();
   constructor(
-    private plantTypesService: PlanttypesService, 
-    private router: Router) { }
+    private plantTypesService: PlanttypesService,
+    private router: Router
+  ) {}
 
   pitchMoist(event: any) {
     this.moistValue = event.value;
     this.plantType.moist = this.moistValue;
   }
-  pitchSun(event:any){
+  pitchSun(event: any) {
     this.sunValue = event.value;
     this.plantType.light = this.sunValue;
   }
-  pitchFrost(event:any){
+  pitchFrost(event: any) {
     this.frostValue = event.value;
     this.plantType.frost = this.fireValue;
   }
-  pitchFire(event:any){
+  pitchFire(event: any) {
     this.fireValue = event.value;
     this.plantType.maxTemp = this.fireValue;
   }
   onSubmit() {
     this.plantTypesService.create(this.plantType);
-    this.router.navigate(['planttypes']);
+    this.router.navigate(["planttypes"]);
   }
-  ngOnInit() {
-  }
+  ngOnInit() {}
 }
