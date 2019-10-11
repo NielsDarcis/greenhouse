268a7316aeb998e3675159b2380252a7061cdac0
NielsDarcis <niels.darcis@gmail.com>
2019-10-11 16:24:00 +0200

    image resizer

--- src/app/plants/plant-detail/plant-detail.component.ts
+++ src/app/plants/plant-detail/plant-detail.component.ts
@@ -32,11 +32,26 @@ export class PlantDetailComponent implements OnInit {
   }
 
  async  onUpload(){
+   // upload image to firebase storage 
     const file = this.selectedFile;
     const filePath = this.selectedFile.name;
     const url = await this.storage.upload(filePath, file).then(async res => await res.ref.getDownloadURL());
-    this.plant.imageUrl = url;
-    this.plantService.update(this.plantId, this.plant);
+    if(url.includes("jpg")){
+      let first = url.split(".jpg",1);
+      let second = url.split(".jpg", 2).splice(1);
+      this.plant.imageUrl = first.toString() +"_400x400.jpg" + second.toString();
+      this.plantService.update(this.plantId, this.plant);
+      this.ngOnInit();
+    }
+    if(url.includes("png")){
+      let first = url.split(".png",1);
+      let second = url.split(".png", 2).splice(1);
+      this.plant.imageUrl = first.toString() +"_400x400.png" + second.toString();
+      this.plantService.update(this.plantId, this.plant);
+      this.ngOnInit();
+    }
+    // refresh page to load in images
+   
   }
 
   async getPlantById() {
