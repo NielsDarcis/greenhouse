3e83d398b6709bfe7c0dab2c1bb27eef0dec6489
fredvdvd <fredvdvd@gmail.com>
2019-10-11 16:51:45 +0200

    filter

--- src/app/plants/plant-types/plant-types.component.ts
+++ src/app/plants/plant-types/plant-types.component.ts
@@ -1,20 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { PlanttypesService } from "../../services/planttypes/planttypes.service";
-import { PlantType } from '../../shared/models/plant-type';
+import { PlantType } from "../../shared/models/plant-type";
+import * as Fuse from 'fuse.js';
+
 @Component({
-  selector: 'app-plant-types',
-  templateUrl: './plant-types.component.html',
-  styleUrls: ['./plant-types.component.scss']
+  selector: "app-plant-types",
+  templateUrl: "./plant-types.component.html",
+  styleUrls: ["./plant-types.component.scss"]
 })
 export class PlantTypesComponent implements OnInit {
   plantTypeList: PlantType[] = [];
+  printList: PlantType[] = [];
+  showFilter: boolean = false;
+  searchFilter: string = "";
   constructor(private plantTypesService: PlanttypesService) {}
-  async getPlants(){
+  
+  async getPlants() {
     this.plantTypeList = await this.plantTypesService.getAll();
-  }
-  async ngOnInit() {
-    await this.getPlants();
-    await console.log(this.plantTypeList)
+    this.printList = this.plantTypeList;
   }
 
+  async ngOnInit() {
+    await this.getPlants();
+  }
+  onfilter() {
+    if(this.searchFilter === ""){
+      this.printList= this.plantTypeList
+      return;
+    }
+    let options: Object = {
+        keys: ['name'],
+        includeMatches: true,
+        findAllMatches: true
+    };
+    let fuse: any = new Fuse(this.plantTypeList, options);
+    let result: any = fuse.search(this.searchFilter);
+    console.log(result)
+    let shortResult:any =[];
+    for(let item of result){
+      shortResult.push(item.item);
+    }
+    this.printList = shortResult;
+  }
 }
