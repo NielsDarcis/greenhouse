5d7cd1dc068565726f2e8e2ad7e8b92019d069a9
fredvdvd <fredvdvd@gmail.com>
2019-11-06 14:35:11 +0100

    planttype tests

--- src/app/plants/plant-types-new/plant-types-new.component.spec.ts
+++ src/app/plants/plant-types-new/plant-types-new.component.spec.ts
@@ -1,25 +1,86 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-// import { PlantTypesNewComponent } from './plant-types-new.component';
+import { PlantTypesNewComponent } from "./plant-types-new.component";
+import { Router, ActivatedRoute, RouterModule } from '@angular/router';
+import { MatDialog, MatSnackBar } from '@angular/material';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ToCelciusPipe } from 'src/app/shared/pipes/to-celcius.pipe';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { MaterialModule } from 'src/app/shared/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-// describe('PlantTypesNewComponent', () => {
-//   let component: PlantTypesNewComponent;
-//   let fixture: ComponentFixture<PlantTypesNewComponent>;
+describe("PlantTypesNewComponent", () => {
+  let component: PlantTypesNewComponent;
+  let fixture: ComponentFixture<PlantTypesNewComponent>;
+  let PLANTTYPES;
+  let mockPlantTypeService;
+  // beforeEach(async(() => {
+  //   TestBed.configureTestingModule({
+  //     declarations: [PlantTypesNewComponent]
+  //   }).compileComponents();
+  //   PLANTTYPES = [
+  //     {
+  //       "-LrnM1Dpw209WMF7Oz8k": {
+  //         id: "-LrnM1Dpw209WMF7Oz8k",
+  //         light: 11,
+  //         maxTemp: 18,
+  //         minTemp: -35,
+  //         moist: 6,
+  //         name: "Eik",
+  //         waterFreq: 2
+  //       }
+  //     },
+  //     {
+  //       "-Lro8uDUPDer2VTdfYmx": {
+  //         id: "-Lro8uDUPDer2VTdfYmx",
+  //         light: 23,
+  //         maxTemp: 60,
+  //         minTemp: 2,
+  //         moist: 50,
+  //         name: "Vijgenboom",
+  //         waterFreq: 4
+  //       }
+  //     },
+  //     {
+  //       "-Lrsl-OYU1hDt7XQf3-o": {
+  //         id: "-Lrsl-OYU1hDt7XQf3-o",
+  //         light: 34,
+  //         maxTemp: 24,
+  //         minTemp: -7,
+  //         moist: 29,
+  //         name: "Ficus",
+  //         waterFreq: 8
+  //       }
+  //     }
+  //   ];
+  //   mockPlantTypeService = jasmine.createSpyObj(['create','update','delete'])
+  //   component = new PlantTypesNewComponent(mockPlantTypeService, Router, ActivatedRoute,MatDialog, MatSnackBar)
+  // }));
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ PlantTypesNewComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        AngularFireDatabaseModule,
+        AngularFireAuthModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig, "greenhouse"),
+        RouterModule.forRoot([]),
+        MaterialModule,
+        BrowserAnimationsModule,
+      ],
+      declarations: [PlantTypesNewComponent, ToCelciusPipe],
+      schemas: [NO_ERRORS_SCHEMA]
+    });
+    fixture = TestBed.createComponent(PlantTypesNewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(PlantTypesNewComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/plants/plant-types-new/plant-types-new.component.ts
+++ src/app/plants/plant-types-new/plant-types-new.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit, Inject } from "@angular/core";
 import { Router, ActivatedRoute } from "@angular/router";
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA
+} from "@angular/material/dialog";
+import { MatSnackBar } from "@angular/material/snack-bar";
 import { PlanttypesService } from "../../services/planttypes/planttypes.service";
 import { PlantType } from "src/app/shared/models/plantType/plant-type";
 import { faTree } from "@fortawesome/free-solid-svg-icons";
@@ -19,8 +23,8 @@ export class PlantTypesNewComponent implements OnInit {
     private router: Router,
     private activeRoute: ActivatedRoute,
     public dialog: MatDialog,
-    private _snackBar: MatSnackBar,
-  ) { }
+    private _snackBar: MatSnackBar
+  ) {}
 
   onSubmit(): void {
     this.plantTypesService.create(this.plantType);
@@ -44,29 +48,28 @@ export class PlantTypesNewComponent implements OnInit {
   }
   openDialog(plantType: PlantType): void {
     const dialogRef = this.dialog.open(PlantTypeNewDialog, {
-      width: '250px',
+      width: "250px",
       data: { plantType: plantType }
     });
   }
   showInfo(text: string, action: string) {
     this._snackBar.open(text, action, {
-      duration: 2000,
+      duration: 2000
     });
   }
-
 }
 
 @Component({
-  selector: 'planttype-new-dialog',
-  templateUrl: 'plant-types-new-dialog.component.html',
+  selector: "planttype-new-dialog",
+  templateUrl: "plant-types-new-dialog.component.html"
 })
 export class PlantTypeNewDialog {
-
   constructor(
     public dialogRef: MatDialogRef<PlantTypeNewDialog>,
     private plantTypesService: PlanttypesService,
     private router: Router,
-    @Inject(MAT_DIALOG_DATA) public data: PlantType) { }
+    @Inject(MAT_DIALOG_DATA) public data: PlantType
+  ) {}
 
   onNoClick(): void {
     this.dialogRef.close();
