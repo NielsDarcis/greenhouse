81e78e937262ccfcbb0e14a7981bf53aab2ae469
NielsDarcis <niels.darcis@gmail.com>
2019-11-06 14:36:09 +0100

    try to write my first unit test

--- src/app/plants/plant-new/plant-new.component.spec.ts
+++ src/app/plants/plant-new/plant-new.component.spec.ts
@@ -1,25 +1,33 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { PlantNewComponent } from './plant-new.component';
+import { PlantNewComponent } from './plant-new.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
 
-// describe('PlantNewComponent', () => {
-//   let component: PlantNewComponent;
-//   let fixture: ComponentFixture<PlantNewComponent>;
+describe('PlantNewComponent', () => {
+  let component: PlantNewComponent;
+  let fixture: ComponentFixture<PlantNewComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ PlantNewComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PlantNewComponent ],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [ AngularFireDatabaseModule, AngularFireAuth,
+        AngularFireModule.initializeApp(environment.firebaseConfig, "greenhouse")]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(PlantNewComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PlantNewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
