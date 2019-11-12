c29964b7093734233ae068d112d90cdcd7d592ce
NielsDarcis <niels.darcis@gmail.com>
2019-11-12 11:06:33 +0100

    firs tests

--- src/app/plants/plant-new/plant-new.component.spec.ts
+++ src/app/plants/plant-new/plant-new.component.spec.ts
@@ -3,9 +3,21 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { PlantNewComponent } from './plant-new.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
-import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireAuth, AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from 'src/environments/environment';
+import { Component, OnInit } from "@angular/core";
+import { PlantsService } from "../../services/plants/plants.service";
+import { PlanttypesService } from "../../services/planttypes/planttypes.service";
+import { Plant } from "../../shared/models/plant/plant";
+import { PlantType } from "../../shared/models/plantType/plant-type";
+import { Router, RouterModule } from "@angular/router";
+import { faSeedling } from "@fortawesome/free-solid-svg-icons";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { RoomsService } from "../../services/rooms/room.service";
+import { Room } from "../../shared/models/room";
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from 'src/app/shared/material.module';
 
 describe('PlantNewComponent', () => {
   let component: PlantNewComponent;
@@ -13,10 +25,17 @@ describe('PlantNewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+        imports: [
+            FormsModule,
+            AngularFireDatabaseModule,
+            AngularFireAuthModule,
+            AngularFireModule.initializeApp(environment.firebaseConfig, "greenhouse"),
+            RouterModule.forRoot([]),
+            MaterialModule,
+          ],
       declarations: [ PlantNewComponent ],
-      schemas: [NO_ERRORS_SCHEMA],
-      providers: [ AngularFireDatabaseModule, AngularFireAuth,
-        AngularFireModule.initializeApp(environment.firebaseConfig, "greenhouse")]
+      schemas: [NO_ERRORS_SCHEMA]
+     
     })
     .compileComponents();
   }));
@@ -29,5 +48,15 @@ describe('PlantNewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    
   });
+
+  // it( 'should render the plant title in the h2 tag', () => {
+  //     fixture.componentInstance.plant = {name:'Jefke'};
+      
+  //     expect(fixture.nativeElement.querySelector('h2').textContent).toContain('Jefke');
+  // } )
+
 });
+
+
--- src/app/plants/plants-list/plants-list.component.spec.ts
+++ src/app/plants/plants-list/plants-list.component.spec.ts
@@ -1,25 +1,77 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { Component, OnInit, ViewChild, NO_ERRORS_SCHEMA } from "@angular/core";
+import { PlantsService } from "../../services/plants/plants.service";
+import { Plant } from "../../shared/models/plant/plant";
+import { MatSort } from "@angular/material/sort";
+import { Router, RouterModule } from "@angular/router";
+import { MatTableDataSource } from "@angular/material/table";
+import { PlantsListComponent } from "./plants-list.component";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFireModule } from "@angular/fire";
+import { environment } from "src/environments/environment";
+import { MaterialModule } from "src/app/shared/material.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
+import { DropMakerComponent } from "src/app/shared/drop-maker/drop-maker.component";
+import { SunMakerComponent } from "src/app/shared/sun-maker/sun-maker.component";
+import { FrostMakerComponent } from "src/app/shared/frost-maker/frost-maker.component";
+import { TimeMakerComponent } from "src/app/shared/time-maker/time-maker.component";
+import { FireMakerComponent } from "src/app/shared/fire-maker/fire-maker.component";
+import { of } from "rxjs";
 
-// import { PlantsListComponent } from './plants-list.component';
+describe("PlantsListComponent", () => {
+  let component: PlantsListComponent;
+  let fixture: ComponentFixture<PlantsListComponent>;
+  let mockHeroService;
+  let PLANTS;
 
-// describe('PlantsListComponent', () => {
-//   let component: PlantsListComponent;
-//   let fixture: ComponentFixture<PlantsListComponent>;
+  beforeEach(() => {
+    PLANTS = [{ name: "Jefke" }];
+    mockHeroService = jasmine.createSpyObj(["getAll"]);
+    TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        AngularFireDatabaseModule,
+        AngularFireAuthModule,
+        AngularFireModule.initializeApp(
+          environment.firebaseConfig,
+          "greenhouse"
+        ),
+        RouterModule.forRoot([]),
+        MaterialModule,
+        BrowserAnimationsModule
+      ],
+      declarations: [
+        PlantsListComponent,
+        DropMakerComponent,
+        SunMakerComponent,
+        FrostMakerComponent,
+        TimeMakerComponent,
+        FireMakerComponent
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [{ provide: PlantsService, useValue: mockHeroService }]
+    }).compileComponents();
+    fixture = TestBed.createComponent(PlantsListComponent);
+  });
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ PlantsListComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  //   beforeEach(() => {
+  //   });
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(PlantsListComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  it("should create", () => {
+    component = fixture.componentInstance;
+    fixture.detectChanges();
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    expect(component).toBeTruthy();
+  });
+
+  it("should set plants correctly from the service", () => {
+    fixture.whenStable().then(() => {
+      let x = mockHeroService.getAll.and.returnValue(PLANTS);
+      fixture.detectChanges();
+      console.log(fixture.componentInstance.plantList, x);
+      expect(fixture.componentInstance.plantList.length).toBe(2);
+    });
+  });
+});
